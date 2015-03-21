35c2cc0b9b5cbe111d2c607a8830ae5255303d6c
okmttdhr <10101114fin@gmail.com>
2015-03-21 16:02:10 +0900

    画像とURLをforeach

--- client/components/home-feed-twitter/home-feed-twitter.controller.js
+++ client/components/home-feed-twitter/home-feed-twitter.controller.js
@@ -5,11 +5,13 @@ angular.module('tweetboxApp')
   .controller('HomeFeedCtrl', function ($scope, Auth, $cookieStore,
     homeFeedTwitter) {
     $scope.tweets;
+    $scope.tweetsLoading = false;
     var currentUser = Auth.getCurrentUser();
     console.log('-----Auth.getCurrentUser()-----');
     console.log(Auth.getCurrentUser());
 
     $scope.updateHomeFeedTwitter = function() {
+      $scope.tweetsLoading = true;
       homeFeedTwitter.index({
         id: '',
         token: currentUser.twitter.token,
@@ -18,8 +20,10 @@ angular.module('tweetboxApp')
         console.log('scc');
         console.log(result);
         $scope.tweets = angular.copy(result);
+        $scope.tweetsLoading = false;      
       }, function(error) {
         console.log('err')
+        $scope.tweetsLoading = false;
       });
     }
   });
