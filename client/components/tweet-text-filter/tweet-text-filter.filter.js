a1a0f0843bd9a1bcc366e46f5cb1d01a50897833
okmttdhr <10101114fin@gmail.com>
2015-03-21 18:46:30 +0900

    整えた

--- client/components/tweet-text-filter/tweet-text-filter.filter.js
+++ client/components/tweet-text-filter/tweet-text-filter.filter.js
@@ -8,31 +8,41 @@ angular.module('tweetboxApp')
     return function (input, mentions, hashtags) {
       var replacedInput;
       var result = "";
-      var index_map = {}
+      var index = {}
 
+      // メンションもハッシュタグもなければそのまま
+      if (mentions.length == 0 && hashtags.length == 0) return input;
+
+      // エスケープ
       function escapeHTML(text) {
         return $('<div/>').text(text).html()
       }
 
-      // ハッシュタグ
-      $.each(hashtags, function(i,item) {
-        index_map[item.indices[0]] = [item.indices[1], function(text) {return "<a target='_blank' href='http://twitter.com/hashtag/"+escapeHTML(item.text)+"'>"+escapeHTML(text)+"</a>"}]
-      })
-
       // メンション
-      $.each(mentions, function(i,item) {
-        index_map[item.indices[0]] = [item.indices[1], function(text) {return "<a target='_blank' href='http://twitter.com/"+escapeHTML(item.screen_name)+"'>"+escapeHTML(text)+"</a>"}]
-      })
+      if (mentions.length != 0) {
+        $.each(mentions, function(i,item) {
+          index[item.indices[0]] = [item.indices[1], function(text) {return "<a target='_blank' href='http://twitter.com/"+escapeHTML(item.screen_name)+"'>"+escapeHTML(text)+"</a>"}]
+        });
+      }
+
+      // ハッシュタグ
+      if (hashtags.length != 0) {
+        $.each(hashtags, function(i,item) {
+          index[item.indices[0]] = [item.indices[1], function(text) {return "<a target='_blank' href='http://twitter.com/hashtag/"+escapeHTML(item.text)+"'>"+escapeHTML(text)+"</a>"}]
+        });
+      }
 
       var last_i = 0
       var i = 0
 
       for (i=0; i < input.length; ++i) {
-        var ind = index_map[i]
+        var ind = index[i]
         if (ind) {
           var end = ind[0]
           var func = ind[1]
-          if (i > last_i) result += escapeHTML(input.substring(last_i, i))
+          if (i > last_i) {
+            result += escapeHTML(input.substring(last_i, i))
+          }
           result += func(input.substring(i, end))
           i = end - 1
           last_i = end
@@ -43,7 +53,9 @@ angular.module('tweetboxApp')
         result += escapeHTML(input.substring(last_i, i))
       }
 
-      console.log(result)
+      // console.log('------------')
+      // console.log(result)
+      // console.log(index)
 
       return result;
     };
