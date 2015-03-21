130fdda57a7257dccc35e0824615c67cd11740ea
okmttdhr <10101114fin@gmail.com>
2015-03-21 21:23:32 +0900

    Filter: tweetTextFilterのテストうまくゆかず。

--- client/components/tweet-text-filter/tweet-text-filter.filter.spec.js
+++ client/components/tweet-text-filter/tweet-text-filter.filter.spec.js
@@ -11,9 +11,39 @@ describe('Filter: tweetTextFilter', function () {
     tweetTextFilter = $filter('tweetTextFilter');
   }));
 
-  it('should return the input prefixed with "tweetTextFilter filter:"', function () {
-    var text = 'angularjs';
-    expect(tweetTextFilter(text)).toBe('tweetTextFilter filter: ' + text);
+  xit('ハッシュタグとメンションがリンクになること', function () {
+    var text = '#Ingress in #Yokohama @tadahiro http://sth';
+    var mentions = [
+      {
+        indices: [
+          { 0: 22 },
+          { 1: 31 }
+        ],
+        screen_name: "tadahiro"
+      }
+    ];
+    var hashtags = [
+      {
+        indices: [
+          { 0: 0 },
+          { 1: 8 }
+        ],
+        text: "Ingress"
+      },
+      {
+        indices: [
+          { 0: 12 },
+          { 1: 21 }
+        ],
+        text: "Yokohama"
+      }
+    ]
+
+    console.log(tweetTextFilter(text, mentions, hashtags))
+    // expect(tweetTextFilter(text, mentions, hashtags)).toBe('<a target="_blank" href="http://twitter.com/hashtag/Ingress">#Ingress</a> in <a target="_blank" href="http://twitter.com/hashtag/Yokohama">#Yokohama</a> <a target="_blank" href="http://twitter.com/tadahiro">@tadahiro</a> http://sth');
+    expect(tweetTextFilter(text, mentions, hashtags)).toBe("<a target='_blank' href='http://twitter.com/hashtag/Ingress'>#Ingress</a> in <a target='_blank' href='http://twitter.com/hashtag/Yokohama'>#Yokohama</a> <a target='_blank' href='http://twitter.com/okmttdhr'>@okmttdhr</a> http://sth");
+    // expect(tweetTextFilter(text, mentions, hashtags)).toBe('#Ingress in #Yokohama @tadahiro http://sth');
+    // expect(tweetTextFilter(text)).toBe('angularjs <a target="_blank" href="http://twitter.com/hashtag/angularhash">#angularhash</a> <a target="_blank" href="http://twitter.com/angularhash">@angularmention>@angularhash</a>');
   });
 
 });
