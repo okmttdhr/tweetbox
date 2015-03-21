07f5bb7b074d4c1a9969ba6b92f29564d8e9d641
okmttdhr <10101114fin@gmail.com>
2015-03-21 21:23:46 +0900

    セミコロン

--- client/components/tweet-text-filter/tweet-text-filter.filter.js
+++ client/components/tweet-text-filter/tweet-text-filter.filter.js
@@ -5,10 +5,13 @@
 // 参考：JavaScript parser for Tweet Entities https://gist.github.com/wadey/442463
 angular.module('tweetboxApp')
   .filter('tweetTextFilter', function () {
-    return function (input, mentions, hashtags, urls) {
+    return function (input, mentions, hashtags) {
       var replacedInput;
       var result = "";
-      var index = {}
+      var index = {};
+
+      console.log(mentions.length);
+      console.log(hashtags.length);
 
       // メンションもハッシュタグもなければそのまま
       if (mentions.length == 0 && hashtags.length == 0) return input;
@@ -26,20 +29,20 @@ angular.module('tweetboxApp')
 
       // エスケープ
       function escapeHTML(text) {
-        return $('<div/>').text(text).html()
+        return $('<div/>').text(text).html();
       }
 
       // メンション
       if (mentions.length != 0) {
         $.each(mentions, function(i,item) {
-          index[item.indices[0]] = [item.indices[1], function(text) {return "<a target='_blank' href='http://twitter.com/"+escapeHTML(item.screen_name)+"'>"+escapeHTML(text)+"</a>"}]
+          index[item.indices[0]] = [item.indices[1], function(text) {return "<a target='_blank' href='http://twitter.com/"+escapeHTML(item.screen_name)+"'>"+escapeHTML(text)+"</a>"}];
         });
       }
 
       // ハッシュタグ
       if (hashtags.length != 0) {
         $.each(hashtags, function(i,item) {
-          index[item.indices[0]] = [item.indices[1], function(text) {return "<a target='_blank' href='http://twitter.com/hashtag/"+escapeHTML(item.text)+"'>"+escapeHTML(text)+"</a>"}]
+          index[item.indices[0]] = [item.indices[1], function(text) {return "<a target='_blank' href='http://twitter.com/hashtag/"+escapeHTML(item.text)+"'>"+escapeHTML(text)+"</a>"}];
         });
       }
 
@@ -47,24 +50,25 @@ angular.module('tweetboxApp')
       var i = 0
 
       for (i=0; i < input.length; ++i) {
-        var ind = index[i]
+        var ind = index[i];
         if (ind) {
-          var end = ind[0]
-          var func = ind[1]
+          var end = ind[0];
+          var func = ind[1];
           if (i > last_i) {
-            result += escapeHTML(input.substring(last_i, i))
+            result += escapeHTML(input.substring(last_i, i));
           }
-          result += func(input.substring(i, end))
-          i = end - 1
-          last_i = end
+          result += func(input.substring(i, end));
+          i = end - 1;
+          last_i = end;
         }
       }
 
       if (i > last_i) {
-        result += escapeHTML(input.substring(last_i, i))
+        result += escapeHTML(input.substring(last_i, i));
       }
 
       // return deleteUrl(result);
+      console.log(result);
       return result;
     };
   });
