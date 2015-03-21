2e719cfdc7d8d1bd1206f1999f28ef6b8bbddcda
okmttdhr <10101114fin@gmail.com>
2015-03-21 21:48:51 +0900

    普通のsplit joinでできてしまった・・

--- client/components/tweet-text-filter/tweet-text-filter.filter.js
+++ client/components/tweet-text-filter/tweet-text-filter.filter.js
@@ -6,69 +6,27 @@
 angular.module('tweetboxApp')
   .filter('tweetTextFilter', function () {
     return function (input, mentions, hashtags) {
-      var replacedInput;
-      var result = "";
-      var index = {};
 
       console.log(mentions.length);
       console.log(hashtags.length);
 
       // メンションもハッシュタグもなければそのまま
       if (mentions.length == 0 && hashtags.length == 0) return input;
-      // if (mentions.length == 0 && hashtags.length == 0) return deleteUrl(input);
-
-      // メンションやハッシュタグとは別の方法でフィルター。
-      // deleteUrlFilterがinjectできなかったため関数にした。
-      // 理由：
-        // URLはテキストから削除したい。
-        // 最後に削除しないとentities に元々はいっている indicesが狂ってしまうため。
-      function deleteUrl(val) {
-        var replacedVal = val.replace(/((http|https|ftp|):\/\/[\w?=&.\/-;#~%-]+(?![\w\s?&.\/;#~%"=-]*>))/g, '');
-        return replacedVal;
-      }
-
-      // エスケープ
-      function escapeHTML(text) {
-        return $('<div/>').text(text).html();
-      }
 
       // メンション
       if (mentions.length != 0) {
         $.each(mentions, function(i,item) {
-          index[item.indices[0]] = [item.indices[1], function(text) {return "<a target='_blank' href='http://twitter.com/"+escapeHTML(item.screen_name)+"'>"+escapeHTML(text)+"</a>"}];
+          input = input.split("@"+item.screen_name).join("<a target='_blank' href='http://twitter.com/"+item.screen_name+"'>@"+item.screen_name+"</a>");
         });
       }
 
       // ハッシュタグ
       if (hashtags.length != 0) {
         $.each(hashtags, function(i,item) {
-          index[item.indices[0]] = [item.indices[1], function(text) {return "<a target='_blank' href='http://twitter.com/hashtag/"+escapeHTML(item.text)+"'>"+escapeHTML(text)+"</a>"}];
+          input = input.split("#"+item.text).join("<a target='_blank' href='http://twitter.com/hashtag/"+item.text+"'>#"+item.text+"</a>");
         });
       }
 
-      var last_i = 0
-      var i = 0
-
-      for (i=0; i < input.length; ++i) {
-        var ind = index[i];
-        if (ind) {
-          var end = ind[0];
-          var func = ind[1];
-          if (i > last_i) {
-            result += escapeHTML(input.substring(last_i, i));
-          }
-          result += func(input.substring(i, end));
-          i = end - 1;
-          last_i = end;
-        }
-      }
-
-      if (i > last_i) {
-        result += escapeHTML(input.substring(last_i, i));
-      }
-
-      // return deleteUrl(result);
-      console.log(result);
-      return result;
+      return input;
     };
   });
