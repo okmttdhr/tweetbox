f2cb56f633c0a7aa94291ad44a25bb10398e35d7
okmttdhr <10101114fin@gmail.com>
2015-03-21 18:11:15 +0900

    ハッシュタグとメンションのパース

--- client/components/tweet-text-filter/tweet-text-filter.filter.js
+++ client/components/tweet-text-filter/tweet-text-filter.filter.js
@@ -3,10 +3,57 @@
 angular.module('tweetboxApp')
   .filter('tweetTextFilter', function () {
     return function (input, mentions, hashtags) {
-      $.each(mentions, function(i, item) {
-        console.log(item)
-        input.split('@'+item.screen_name).join('<a href="http://twitter.com/'+item.screen_name+'" target="_blank">@'+item.screen_name+'</a>');
+      var replacedInput;
+      var result = "";
+      var index_map = {}
+
+      function escapeHTML(text) {
+        return $('<div/>').text(text).html()
+      }
+
+      // "#"を使うとうまくいかなかったのでTwitterのURLに実際のクエリとして出る"%23"を使用
+      $.each(hashtags, function(i,item) {
+        index_map[item.indices[0]] = [item.indices[1], function(text) {return "<a target='_blank' href='http://twitter.com/search?q="+escapeHTML("%23"+item.text)+"'>"+escapeHTML(text)+"</a>"}]
       })
-      return input;
+
+      // メンション
+      $.each(mentions, function(i,item) {
+        index_map[item.indices[0]] = [item.indices[1], function(text) {return "<a target='_blank' href='http://twitter.com/"+escapeHTML(item.screen_name)+"'>"+escapeHTML(text)+"</a>"}]
+      })
+
+      var last_i = 0
+      var i = 0
+
+      for (i=0; i < input.length; ++i) {
+        var ind = index_map[i]
+        if (ind) {
+          var end = ind[0]
+          var func = ind[1]
+          if (i > last_i) result += escapeHTML(input.substring(last_i, i))
+          result += func(input.substring(i, end))
+          i = end - 1
+          last_i = end
+        }
+      }
+
+      if (i > last_i) {
+        result += escapeHTML(input.substring(last_i, i))
+      }
+
+      console.log(result)
+
+      // if (mentions.length != 0) {
+      //   $.each(mentions, function(i, item) {
+      //     console.log(item)
+      //     console.log(input)
+      //     replacedInput = input.split(item.screen_name).join('<a href="http://twitter.com/'+item.screen_name+'" target="_blank">'+item.screen_name+'</a>');
+      //     // replacedInput = input.replace(/((http|https|ftp):\/\/[\w?=&.\/-;#~%-]+(?![\w\s?&.\/;#~%"=-]*>))/g, '');
+      //
+      //     // input.split('@'+item.screen_name).join('<a href="http://twitter.com/'+item.screen_name+'" target="_blank">@'+item.screen_name+'</a>');
+      //     // console.log(test)
+      //   })
+      // }
+      // return replacedInput;
+      return result;
     };
   });
